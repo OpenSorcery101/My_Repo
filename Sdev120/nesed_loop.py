db3168b3c83243df990cd52a85dfaab17f83c25e
OpenSorcery101 <22rosss@gmail.com>
2022-09-06 13:07:13 -0400

    Clean

---
+++ Sdev120/nesed_loop.py
@@ -0,0 +1,10 @@
+counter = 1000
+
+while True:
+    counter = counter - 1
+    print("Piplip go burrr")
+    if counter == 0:
+        print("Piplup Has Evolved")
+        break
+    else:
+        continue
