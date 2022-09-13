1fc78fca8acb15791a9de035b965f5288216d33f
OpenSorcery101 <22rosss@gmail.com>
2022-09-13 14:52:10 -0400

    change

--- Sdev120/Home_Work/Gusseing_game.py
+++ Sdev120/Home_Work/Gusseing_game.py
@@ -1,36 +1,31 @@
 import random 
-
+import os   
 
 number = random.randint(1,100)
-print(number)
 
-
-counter = 6
-while counter > 1:
-    counter = counter - 1 
-    user_number = int(input("Please enter a whole number!: "))
-    print("You have " + str(counter) + " guesses left!")
-    if user_number < 1:
-        print("Please enter a whole number equal to or greater to 1")
-        continue
-        break
-    elif user_number > 100:
-        print("please enter a whole number equal to or less than 100")
-        continue
-        break
-    elif counter == 1:
-        print("You ran out of attempts!")
-        print("The magic number is " + str(number))
-        break
-    elif user_number == number:
-        print("You Win! \nThe magic number was " + str(number))
-        break
-    elif user_number < number:
-        print("Your number is  to high!")
-        continue
-        break
-    elif user_number > number:
-        print("You number was to low!")
-        continue
-        break
-
+question = "y"
+counter = 0 
+while counter < 5 or question =="y": 
+    number = number
+    counter = counter + 1
+    user_num = int(input("Please enter a whole number between 1 - 100: "))
+    if counter == 5:
+            os.system('cls')
+            print("You ran out of gusses the magic number was " + str(number))
+            question = input("Would you like to play again? y|N ").lower
+            number = random.randint(1,100)
+    else:
+        if user_num >= 1 and user_num <= 100:
+            if user_num > number and counter <= 5:
+                os.system('cls')
+                print("your number is to high! Try Again!\n" + "You have used " + str(counter) +" /5 Gusses" )
+            elif user_num < number and counter <=5:
+                os.system('cls')
+                print("You number is to low! Try Again!\n"+ "You have used " + str(counter) +" /5 Gusses")
+            elif user_num  == number and counter <=5:
+                os.system('cls')
+                print("You found the magic number!")
+            else:
+                print("second no")
+        else:
+         print("No")
